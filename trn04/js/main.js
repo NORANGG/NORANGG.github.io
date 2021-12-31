8c5c7a43673f20785be4950aa0feebc460dc4074
NORANGG <91935238+NORANGG@users.noreply.github.com>
2021-12-31 17:29:32 +0900

    Add files via upload

--- trn04/js/main.js
+++ trn04/js/main.js
@@ -1,8 +1,28 @@
 $(function(){
 //---------------------------------------------------------------
-$(window).on('scroll', ()=>{            /*<()=>이게 펑션 대신쓰는거*/
+$('#popup').draggable();
+
+$('#popup a').on('click', function(){
+    $(this).parent().fadeOut();
+    return false;
+});
+
+
+
+$('.search').on('click', function(){
+    $('.search_form').stop().slideToggle();
+});
+
+
+
+
+$(window).on('scroll', ()=>{ /*<()=>이게 펑션 대신쓰는거*/
     let sct=$(window).scrollTop();
+    console.log(sTop,sct);
     sct > 0 ? $('#header').addClass('on') : $('#header').removeClass('on');
+    //if(sct > 0) {$('#header').addClass('on')}else{$('#header').removeClass('on')}
+    sct > 300 ? $('#to_top').fadeIn() : $('#to_top').fadeOut();
+    sct > sTop ? $('#solution').addClass('on') : $('#solution').removeClass('on');
 });
 
 
@@ -43,7 +63,21 @@ $('.pr_slider').on('afterChange', function(e,s,c){
 
 
 
+$('.familylink .link>a').on('click', function(e){
+    /*e.preventDefault();  <-a태그에 #!안해도 이거해놓으면 홈위로 안감*/
+    $(this).prev().stop().slideToggle();
+    $(this).find('i').toggleClass('on')
+    return false;
+});
 
+
+
+$('#to_top a').on('click', e=>{
+    e.preventDefault();
+    $('html,body').animate({scrollTop:0},1000,)
+});
+
+let sTop = $('#solution').offset().top - 200;
 
 
 
