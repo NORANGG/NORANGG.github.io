36b4916774f03a32958938f34c98c68e182ff03d
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-01-13 18:58:04 +0900

    Add files via upload

--- portfolio/pf_portfolio.js
+++ portfolio/pf_portfolio.js
@@ -7,8 +7,9 @@ $('#main').fullpage({
     anchors:['intro','portfolio_01','portfolio_02','portfolio_03','portfolio_04','portfolio_05','profile',],              /*풀페이지에서 페이지 번호 정해주기*/
     afterLoad:function(origin, destination, direction){
         let idx=destination.index;                                 /*<-afterLoad가 실행될때 이름을 붙이는거라 안쪽에 넣고 섹션은 매번 해야하니까 바깥쪽에!*/
-        sc.eq(idx).addClass("on").siblings().removeClass('on');
+        sc.eq(idx).addClass("on").siblings().removeClass('on');  /*<-섹션에 on붙였다 떼기*/
         sideBar.eq(idx).addClass("on").siblings().removeClass('on');  /*<-옆에 네비바(사이드바) on붙였다 떼는거*/
+        // sct > sTop ? $('.section').addClass('on') : $('.section').removeClass('on');
     },
 
 });
@@ -34,5 +35,9 @@ $('#cover').append(cloneMenu);
 $('#cover').on('scroll wheel touchmove', function(){
     return false;
 });
+
+
+
+
 //-----------------------------------------------------------------
 });
