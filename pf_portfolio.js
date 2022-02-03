791240495d2553b8536edb49fc6e623264ebcbc0
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-02-03 15:49:12 +0900

    Add files via upload

--- pf_portfolio.js
+++ pf_portfolio.js
@@ -15,17 +15,22 @@ $('#main').fullpage({
 
 
 
-$('.portfolio_01 .guide i').click(function(){
-    $('body').addClass('scrollDisable').on('scroll touchmove mousewheel', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-    });
-    });
+// $('.portfolio_01 .guide i').click(function(){
+//     $('body').addClass('scrollDisable').on('scroll touchmove mousewheel', function(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     return false;
+//     });
+//     });
 
-    $('.portfolio_01 .guide i.xi-close').click(function(){
-    $('body').removeClass('scrollDisable').off('scroll touchmove mousewheel');
-});
+//     $('.portfolio_01 .guide i.xi-close').click(function(){
+//     $('body').removeClass('scrollDisable').off('scroll touchmove mousewheel');
+// });
+
+
+$('.guide figure').on('scroll wheel', function(e){
+    e.stopPropagation();
+})
 
 
 new Typed('.slogan_type',{
