a0c0faebfbe500eccfe584990e830e5f1f77a544
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-03-11 17:55:52 +0900

    Add files via upload

--- alone_01/js/alone_01.js
+++ alone_01/js/alone_01.js
@@ -6,7 +6,38 @@ $(function(){
         autoplaySpeed:5000,
         pauseOnHover:false,
         fade:true,
+        dots:false,
     });
     
+
+
+    $('#content_03 .slider>div').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:4000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        vertical:true,
+        slidesToShow: 1,
+        responsive: [
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+    
+          {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+    
+    
+    
+        ]
+      
+      });
 //------------------------------------------------------------------
 });
