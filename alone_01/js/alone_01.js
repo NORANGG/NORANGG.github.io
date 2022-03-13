277fa6f0a93fa2dfb03c4d01d02812b30f10fc41
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-03-13 16:33:18 +0900

    Add files via upload

--- alone_01/js/alone_01.js
+++ alone_01/js/alone_01.js
@@ -39,5 +39,25 @@ $(function(){
         ]
       
       });
+
+
+
+
+
+      $('.gnb>ul>li>a').on('click', function(e){
+        if($('.gnb').hasClass('on')) {
+            e.preventDefault();                                /*<-내가 가지고 있는 이벤트를 없애는 것. html에서 a href="#"에 #대신 주소쓰면 거기로가잖아 근데 그거 무효화시키는거임*/
+            /*$('.sub_menu').hide();*/
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('.sub_menu').stop().slideUp();
+        }
+        
+    });
+
+
+    $('.m_open').on('click', function(){
+      $('.gnb').toggleClass('on');
+      // $('.sns').fadeToggle();                    /*<-toggle은 사라진다, fadetoggle은 나타났다사라진다*/
+  });
 //------------------------------------------------------------------
 });
