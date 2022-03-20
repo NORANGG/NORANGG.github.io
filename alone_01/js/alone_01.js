602c798cd3599424159380a2673d8210ae0ba423
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-03-20 17:45:50 +0900

    Add files via upload

--- alone_01/js/alone_01.js
+++ alone_01/js/alone_01.js
@@ -43,21 +43,25 @@ $(function(){
 
 
 
-
-      $('.gnb>ul>li>a').on('click', function(e){
-        if($('.gnb').hasClass('on')) {
-            e.preventDefault();                                /*<-내가 가지고 있는 이벤트를 없애는 것. html에서 a href="#"에 #대신 주소쓰면 거기로가잖아 근데 그거 무효화시키는거임*/
-            $('.sub_menu').hide();
-            $(this).next().stop().slideToggle();
-            $(this).parent().siblings().find('.sub_menu').stop().slideUp();
-        }
-        
+    $(function(){
+      $('nav .depth01>li').on('click', function(){   /*nav .depth01>li 를 클릭 했을때*/
+          if($('nav').hasClass('on')) {   /*만약 nav에 on이 붙었으면*/
+              $('.sub_menu').stop().slideUp();   /*섭메뉴는 슬라이드 업되고 (그래야 다른메뉴 눌렀을때 그전에 열린게 닫힘)*/
+              $(this).find('.sub_menu').stop().slideToggle();   /*내(this,즉 nav .depth01>li임) 아래에 있는 섭메뉴는 나오게함*/
+          }
+      });
     });
 
 
     $('.m_open').on('click', function(){
-      $('.gnb').toggleClass('on');
+      $('nav').toggleClass('on');
       // $('.sns').fadeToggle();                    /*<-toggle은 사라진다, fadetoggle은 나타났다사라진다*/
-  });
+    });
+
+
+ $(window).on('resize',function(){   /*윈도우가 리사이즈 될떄 섭메뉴의 스타일이 빠지고 네비의 on이 빠진다*/
+        $('.sub_menu').removeAttr('style');
+        $('nav').removeClass('on'); 
+    });
 //------------------------------------------------------------------
 });
