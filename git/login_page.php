2a66c5e2c721ce5382eabd5c41dca370fadbaef3
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-05-30 11:12:24 +0900

    Delete login_page.php

--- git/login_page.php
+++
@@ -1,9 +0,0 @@
-<form action="login_page.php" method="get">
-
-    아이디 : <input type="text" name="uid" /></br>
-
-    비밀번호 : <input type="password" name="pwd" /></br>
-
-    <input type="submit" value="GET방식으로 전송" />
-
-</form>
