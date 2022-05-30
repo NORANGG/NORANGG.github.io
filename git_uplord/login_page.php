8e3a4c3c4a3a1dcbd454c87271d15a428345ba9b
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-05-30 11:15:17 +0900

    Add files via upload

---
+++ git_uplord/login_page.php
@@ -0,0 +1,11 @@
+<form action="login_page2.php" method="get">
+
+    아이디 : <input type="text" name="uid" /></br>
+
+    비밀번호 : <input type="password" name="pwd" /></br>
+
+    <input type="submit" value="GET방식으로 전송" />
+
+</form>
+
+
