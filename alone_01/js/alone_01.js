3032d2d7cb0352a60f8163bb7c6b5162ad8c8372
NORANGG <91935238+NORANGG@users.noreply.github.com>
2022-10-17 13:33:43 +0900

    Add files via upload

--- alone_01/js/alone_01.js
+++ alone_01/js/alone_01.js
@@ -44,10 +44,10 @@ $(function(){
 
 
     $(function(){
-      $('nav .depth01>li').on('click', function(){   /*nav .depth01>li 를 클릭 했을때*/
-          if($('nav').hasClass('on')) {   /*만약 nav에 on이 붙었으면*/
-              $('.sub_menu').stop().slideUp();   /*섭메뉴는 슬라이드 업되고 (그래야 다른메뉴 눌렀을때 그전에 열린게 닫힘)*/
-              $(this).find('.sub_menu').stop().slideToggle();   /*내(this,즉 nav .depth01>li임) 아래에 있는 섭메뉴는 나오게함*/
+      $('nav .depth01>li').on('click', function(){   
+          if($('nav').hasClass('on')) {   
+              $('.sub_menu').stop().slideUp();   
+              $(this).find('.sub_menu').stop().slideToggle();   
           }
       });
     });
@@ -55,11 +55,11 @@ $(function(){
 
     $('.m_open').on('click', function(){
       $('nav').toggleClass('on');
-      // $('.sns').fadeToggle();                    /*<-toggle은 사라진다, fadetoggle은 나타났다사라진다*/
+      // $('.sns').fadeToggle();                    
     });
 
 
- $(window).on('resize',function(){   /*윈도우가 리사이즈 될떄 섭메뉴의 스타일이 빠지고 네비의 on이 빠진다*/
+  $(window).on('resize',function(){   
         $('.sub_menu').removeAttr('style');
         $('nav').removeClass('on'); 
     });
